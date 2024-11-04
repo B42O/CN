1801f3bbce1879fd243f770096c3f8b84110c650
B42O <uvbrothers21@gmail.com>
2024-11-04 10:04:55 +0530

    Update 6.java

--- 6.java
+++ 6.java
@@ -1,43 +1,41 @@
 import java.util.*;
-public class Main6 {
+public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the number of nodes:");
-        int n = s.nextInt(); 
-        int[][] g = new int[n][n]; 
-        System.out.println("Enter the adjacency matrix:");
+        System.out.print("Enter number of nodes: ");
+        int n = s.nextInt();
+        int[][] g = new int[n][n];
+        System.out.println("Enter the distance matrix (use 9999 for infinity):");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 g[i][j] = s.nextInt();
             }
         }
-        System.out.println("Enter the source node:");
-        int src = s.nextInt(); 
-        int[] d = dijkstra(g, src);
-        System.out.println("Shortest distances:");
-        for (int i = 0; i < n; i++) {
-            System.out.println(src + " -> " + i + ": " + d[i]);
+        System.out.print("Enter source node: ");
+        int src = s.nextInt();
+        int[] d = new int[n];
+        boolean[] v = new boolean[n];
+        Arrays.fill(d, 9999);
+        d[src] = 0;
+        for (int c = 0; c < n - 1; c++) {
+            int u = -1;
+            for (int i = 0; i < n; i++) {
+                if (!v[i] && (u == -1 || d[i] < d[u])) {
+                    u = i;
+                }
+            }
+            v[u] = true;
+            for (int j = 0; j < n; j++) {
+                if (!v[j] && g[u][j] != 9999 && d[u] + g[u][j] < d[j]) {
+                    d[j] = d[u] + g[u][j];
+                }
+            }
         }
-        s.close(); 
-    }
-    static int[] dijkstra(int[][] g, int src) {
-    int n = g.length;
-    int[] d = new int[n];
-    boolean[] v = new boolean[n];
-    for (int i = 0; i < n; i++) {
-        d[i] = 9999;
-    }
-    d[src] = 0;
-    for (int i = 0; i < n; i++) {
-        int u = -1;
-        for (int j = 0; j < n; j++)
-            if (!v[j] && (u == -1 || d[j] < d[u])) u = j;
-        v[u] = true;
-        for (int j = 0; j < n; j++)
-            if (!v[j] && g[u][j] != 0)
-                d[j] = Math.min(d[j], d[u] + g[u][j]);
-    }
-    return d;
+        System.out.println("Shortest distances from node " + src + ":");
+        for (int i = 0; i < n; i++) {
+            System.out.println("To " + i + " - " + (d[i] == 9999 ? "Infinity" : d[i]));
+        }
+        s.close();
     }
 }
 Enter the number of nodes:
